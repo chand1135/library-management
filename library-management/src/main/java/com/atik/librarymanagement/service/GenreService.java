a60381161817a02bacf065badc629212a8bcc33d
haque <haque@LAPTOP-IRF5H9SN>
2023-03-28 05:15:52 +0530

    Genre CRUD API created

---
+++ library-management/src/main/java/com/atik/librarymanagement/service/GenreService.java
@@ -0,0 +1,23 @@
+package com.atik.librarymanagement.service;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import com.atik.librarymanagement.model.Genre;
+
+@Component
+public interface GenreService {
+	
+	public HttpStatus create(Genre genre) throws IllegalArgumentException;
+	
+	public List<Genre> getGenres();
+	
+	public Genre getGenre(String id) throws IllegalArgumentException;
+	
+	public HttpStatus deleteGenre(String id);
+	
+	public HttpStatus update(Genre genre) throws IllegalArgumentException;
+
+}
